ac720933af4e5aa1f2e21f2012901df661427562
Dmitriy Shafalyuk <dima.shafalyuk@mail.ru>
2023-05-14 23:08:35 +0300

    firstCommit

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Movies } from '../components/Movies';
+import { Preloader } from '../components/Preloader';
+import { Search } from '../components/Search';
 
 class Main extends React.Component {
   state = {
@@ -8,21 +10,23 @@ class Main extends React.Component {
 
   componentDidMount() {
     fetch('http://www.omdbapi.com/?apikey=e5430642&s=MATRIX')
-      .then(response => response.json())
-      .then(data => this.setState({ movies: data.Search }));
+      .then((response) => response.json())
+      .then((data) => this.setState({ movies: data.Search }));
   }
+
+  searchMovies = (str) => {
+    fetch(`http://www.omdbapi.com/?apikey=e5430642&s=${str}`)
+      .then((response) => response.json())
+      .then((data) => this.setState({ movies: data.Search }));
+  };
 
   render() {
     const { movies } = this.state;
 
     return (
       <main className="container content">
-        {
-        movies.length ? (
-          <Movies movies={this.state.movies} />
-        ) : 
-          <h3>Loadind...</h3>
-        }
+        <Search searchMovies={this.searchMovies} />
+        {movies.length ? <Movies movies={this.state.movies} /> : <Preloader />}
       </main>
     );
   }
