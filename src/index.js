57edb9c07ed379f776975a82ce5ba576850c7405
Dmitriy Shafalyuk <dima.shafalyuk@mail.ru>
2023-05-16 23:24:23 +0300

    firstCommit

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-ReactDOM.render();
-render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
