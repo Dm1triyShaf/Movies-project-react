6a006e4ebff5fc42f84c8a44f2cc3e71baca00f6
Dmitriy Shafalyuk <dima.shafalyuk@mail.ru>
2023-05-16 12:21:22 +0300

    firstCommit

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,12 +3,17 @@ import React from 'react';
 class Search extends React.Component {
   state = {
     search: '',
+    tupe: 'all',
   };
 
   handleKey = (event) => {
     if (event.key === 'Enter') {
       this.props.searchMovies(this.state.search);
     }
+  };
+
+  handleFilter = (event) => {
+    this.setState({ type: event.target.dataset.type });
   };
 
   render() {
@@ -23,6 +28,38 @@ class Search extends React.Component {
             onChange={(e) => this.setState({ search: e.target.value })}
             onKeyDown={this.handleKey}
           />
+        </div>
+        <div>
+          <label>
+            <input
+              class="with-gap"
+              name="type"
+              type="radio"
+              data-type="all"
+              onChange={this.handleFilter}
+            />
+            <span>All</span>
+          </label>
+          <label>
+            <input
+              class="with-gap"
+              name="type"
+              type="radio"
+              data-type="movie"
+              onChange={this.handleFilter}
+            />
+            <span>Movies only</span>
+          </label>
+          <label>
+            <input
+              class="with-gap"
+              name="type"
+              type="radio"
+              data-type="series"
+              onChange={this.handleFilter}
+            />
+            <span>Series Only</span>
+          </label>
         </div>
       </div>
     );
