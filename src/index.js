4639597692ba15886bdbbad869f6874270d85f50
Dmitriy Shafalyuk <dima.shafalyuk@mail.ru>
2023-05-16 23:21:21 +0300

    firstCommit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render();
+render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 
