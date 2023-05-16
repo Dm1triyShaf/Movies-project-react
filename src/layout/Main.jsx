d18990971d15675c14eec73c7132aa5fb3610a88
Dmitriy Shafalyuk <dima.shafalyuk@mail.ru>
2023-05-16 14:19:49 +0300

    firstCommit

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -3,7 +3,7 @@ import { Movies } from '../components/Movies';
 import { Preloader } from '../components/Preloader';
 import { Search } from '../components/Search';
 
-const API_KEY = process.evn.REACT_APP_API_KEY;
+const API_KEY = process.evn.REACT_API_KEY;
 
 class Main extends React.Component {
   state = {
@@ -14,13 +14,11 @@ class Main extends React.Component {
   componentDidMount() {
     fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=MATRIX`)
       .then((response) => response.json())
-      .then((data) => 
-          this.setState({ movies: data.Search, loading: false })
-          )
-          .catch((err) => {
-            console.error(err);
-            this.setState({loading:false})
-          })
+      .then((data) => this.setState({ movies: data.Search, loading: false }))
+      .catch((err) => {
+        console.error(err);
+        this.setState({ loading: false });
+      });
   }
 
   searchMovies = (str, type = 'all') => {
@@ -31,12 +29,11 @@ class Main extends React.Component {
       }`
     )
       .then((response) => response.json())
-      .then((data) => this.setState({ movies: data.Search, loading: false })
-      )
+      .then((data) => this.setState({ movies: data.Search, loading: false }))
       .catch((err) => {
         console.error(err);
-        this.setState({loading:false})
-      })
+        this.setState({ loading: false });
+      });
   };
 
   render() {
